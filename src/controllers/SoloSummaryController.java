88bc102dc973131f256bc3b396d5ce7b3b5e40e7
Bartek <bartek.jachowicz@gmail.com>
2017-05-31 00:06:16 +0200

    Solo game summary screen added.

---
+++ src/controllers/SoloSummaryController.java
@@ -0,0 +1,63 @@
+package controllers;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import logic.CategoriesSet;
+
+import java.io.IOException;
+
+public class SoloSummaryController {
+    public Button button1;
+    public Button button2;
+    public Button button3;
+
+    private MainController mainController;
+
+    public void setMainController(MainController mainController) {
+        this.mainController = mainController;
+    }
+
+    @FXML
+    public void initialize() {
+        button1.setText("Play Again");
+        button2.setText("Ranking");
+        button3.setText("EXIT");
+    }
+
+    @FXML
+    public void backToMainMenu(ActionEvent actionEvent) {
+        System.out.println(actionEvent);
+        mainController.reinitialize();
+    }
+    @FXML
+    public void seeRankings(ActionEvent actionEvent) {
+        System.out.println(actionEvent);
+        mainController.reinitialize();
+    }
+    @FXML
+    public void playAgain(ActionEvent actionEvent) {
+        System.out.println(actionEvent);
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/SoloPrepare.fxml"));
+        Pane pane = null;
+
+        try {
+            pane = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        SoloPrepareController controller = loader.getController();
+        controller.setMainController(mainController);
+        mainController.addToStackPane(pane);
+    }
+
+    @FXML
+    public void exitPressed(ActionEvent actionEvent) {
+        System.out.println(actionEvent);
+        Platform.exit();
+    }
+}
